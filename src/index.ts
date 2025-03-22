e3627356aa327531e091ea9b42d6c13822becdb9
aisyahilma <aisyahilmaderajat@gmail.com>
2025-03-22 15:38:51 +0800

    Initialize project with PostgreSQL setup, API endpoints, and Prisma integration

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import { Hono } from "hono";
+import { prisma } from "./lib/prisma";
+
+const app = new Hono();
+
+app.get("/", (c) => {
+  return c.json({
+    message: "Cerdiq Food Backend REST API",
+  });
+});
+
+app.get("/products", async (c) => {
+  const products = await prisma.product.findMany();
+
+  return c.json(products);
+});
+
+export default app;
